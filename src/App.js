40a59c4b260d8839cb2c002d89fdc0e236c3b99c
mahmoudbachir <mahmoud@mahmoudbachir.com>
2017-06-08 18:47:59 -0700

    Connected react to firebase database

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import React, { Component } from 'react';
-import { firebase, auth } from './utils/firebase';
+import * as firebase from 'firebase';
 
 class App extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
+      camels: 42,
       currUser: null
     }
   }
 
   componentWillMount() {
-    auth.onAuthStateChanged(currentUser => {
-      if (currentUser) {
-        console.log('Logged in:', currentUser);
-        this.setState({ currentUser });
-      } else {
-        this.setState({ currentUser: null });
-      }
-    });
+    // auth.onAuthStateChanged(currentUser => {
+    //   if (currentUser) {
+    //     console.log('Logged in:', currentUser);
+    //     this.setState({ currentUser });
+    //   } else {
+    //     this.setState({ currentUser: null });
+    //   }
+    // });
+  }
+
+  componentDidMount() {
+    const rootRef = firebase.database().ref();
+    const camelsRef = rootRef.child('camels');
+    // 'on' method synchronizes data in real time
+    // attach it onto a reference that points to a place in the database
+
+    // so when the database makes a change, make that update to our react state in real time
+    camelsRef.on('value', snap => {
+        this.setState({
+          camels: snap.val()
+        })
+    })
   }
 
   loginButtonClicked(e) {
@@ -35,7 +50,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Welcome To Weekly Grind I Guess</h1>      
+        <h1>Welcome To Weekly Grind I Guess...</h1> 
+        <h2>fake camels is {this.state.camels}</h2>     
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './css/main.css';
+import * as firebase from 'firebase';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// TODO: replace with your project's customized code snippet
+const config = {
+    apiKey: "AIzaSyCYgvWD7rIPlWxuGtD0WqnOTY3Y0Q31-8I",
+    authDomain: "weekly-grind.firebaseapp.com",
+    databaseURL: "https://weekly-grind.firebaseio.com",
+    storageBucket: "weekly-grind.appspot.com"
+};
+
+// initialize firebase app with config information
+firebase.initializeApp(config);
+
+ReactDOM.render(
+		<App />, 
+		document.getElementById('root')
+	);
 registerServiceWorker();
