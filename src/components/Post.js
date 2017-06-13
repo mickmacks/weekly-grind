bef41adba31535b1af171aa8c7a98eb432e28898
mahmoudbachir <mahmoud@mahmoudbachir.com>
2017-06-12 20:52:16 -0700

    polished card styling and updated images

--- src/components/Post.js
+++ src/components/Post.js
@@ -56,7 +56,7 @@ class Post extends Component {
 						<h3 className="post-info-username">{this.state.username}</h3>
 						<h4 className="post-info-location">{this.state.location}</h4>
 					</div>
-					<h5 className="post-likes-count">{this.props.post.likesCount}</h5>
+					<h5 className="post-likes-count"><img src="./images/heart.png" /> {this.props.post.likesCount}</h5>
 				</div>
 			</div>
 		)
