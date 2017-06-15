0e35651af09f74c9b0f69a6416fdeb51d0cca4b6
mahmoudbachir <mahmoud@mahmoudbachir.com>
2017-06-14 19:23:04 -0700

    trying to fix weird footer naming issue

--- src/components/footer.js
+++ src/components/footer.js
@@ -15,7 +15,7 @@ class Footer extends Component {
 						<li>© 2017 Mahmoud Bachir</li>
 					</ul>
 					<div id="footer-blurb">
-						<p>Welcome to the Weekly Grind! We are a community of thousands, helping eachother to develop and grow in small pockets. Create a private group, or join a public one near you. Learn, develop and share your experiences with your friends and peers — and all for free!</p>
+						<p>Welcome to the Weekly Grind! We are an international community of thousands, helping eachother to develop and grow in small pockets. Create a private group, or join a public one near you. Learn, develop and share your experiences with your friends and peers — and all for free!</p>
 					</div>
 				</div>
 			</footer>
